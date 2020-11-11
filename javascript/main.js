5e3475b9f432de63caa878e356178c5e8c4a98e7
Dzastinche <63582403+Dzastinche@users.noreply.github.com>
2020-11-11 11:50:25 +0100

    idemo

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-let everything=[]
+let matches=[]
 let comp=[]
 let Today=new Date()
 let year=Today.getFullYear();
@@ -7,9 +7,10 @@ let mjesec=Today.getMonth()+1
 if (day<10){
 	day="0"+day;
 }
-console.log(year,day,mjesec)
+//Fetching information
+
 const proxy='https://shrouded-temple-39982.herokuapp.com/';
-fetch(`${proxy}https://api.football-data.org/v2/matches?dateFrom=${year}-${mjesec}-06&dateTo=${year}-${mjesec}-07`,
+fetch(`${proxy}https://api.football-data.org/v2/matches?dateFrom=${year}-${mjesec}-${day+8 }&dateTo=${year}-${mjesec}-${day+10}`,
 {
 	"method": "GET",
 		"headers": {
@@ -18,22 +19,25 @@ fetch(`${proxy}https://api.football-data.org/v2/matches?dateFrom=${year}-${mjese
 .then(e=>e.json())
 .then(er=>{
 	console.log(er)
-	everything.push(er.matches)
-	return everything})
+	matches.push(er.matches)
+	return matches})
+//Destructing Competition
+
 .then(e=>{
 	for(let i=0;i<e[0].length;i++){
 		comp.push(e[0][i].competition)
 	}
-	return everything
+	return matches
 })
-
 const Teams=(t)=>{
+	//ELements
 	let team=document.createElement('div')
 	team.className='container-match-result-first';
 	team.innerText=t.homeTeam.name
 	let team2=document.createElement('div')
 	team2.className='container-match-result-seccond'
 	team2.innerText=t.awayTeam.name;
+	//Check length
 	if(team.innerText.length>16){
 		team.innerText=team.innerText.substring(0,15)+ "...";
 		team.classList.add('lower')
@@ -67,7 +71,7 @@ const Teams=(t)=>{
 	finnaly.innerHTML=team.outerHTML + spans.outerHTML +time.outerHTML +team2.outerHTML;
 	return finnaly
 }
-
+// Create Competition item
 const Comp=(competitions)=>{
 	const li=document.createElement('li');
 	li.className='container-match';
@@ -81,14 +85,13 @@ const Comp=(competitions)=>{
 	lista.append(head,li)
 	const rezultat=document.createElement('div');
 	rezultat.className='container-item'
-	rezultat.setAttribute('key',competitions.id)
 	rezultat.appendChild(lista);
 	document.querySelector('#container UL').appendChild(rezultat)
-			for(let j=0;j<everything[0].length;j++)
+			for(let j=0;j<matches[0].length;j++)
 			{
 				
-				if(everything[0][j].competition.id===competitions.id){
-					li.appendChild(Teams(everything[0][j]))
+				if(matches[0][j].competition.id===competitions.id){
+					li.appendChild(Teams(matches[0][j]))
 				}}
 		
 }
@@ -105,12 +108,8 @@ const Comp=(competitions)=>{
 			 };
 			check.push(comp[i].id)
 			}
-			comp.length>1?loader.classList.add('hidden'):loader.classList.remove('hidden')
+			comp.length>0?loader.classList.add('hidden'):loader.classList.remove('hidden')
 		}
-		,1500)
+		,2000)
 let loader=document.getElementById('load')
-/*window.addEventListener('load',()=>{
-	setTimeout(()=>{
-		loader.classList.add('hidden')
-	},3100)
-})*/
+
